05fe28d88a6208bf5cca245933b9542e8601ca19
Jan Ouwens <dimbouzoukas@gmail.com>
2022-07-30 12:29:31 +0300

    Updated lsp handlers and null-ls

--- nvim/lua/plugins/lsp/handlers.lua
+++ nvim/lua/plugins/lsp/handlers.lua
@@ -1,4 +1,14 @@
 local M = {}
+local M = {}
+
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+
+local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not status_cmp_ok then
+  return
+end
+M.capabilities.textDocument.completion.completionItem.snippetSupport = true
+M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
 
 M.setup = function()
   local signs = {
--- nvim/lua/plugins/lsp/null-ls.lua
+++ nvim/lua/plugins/lsp/null-ls.lua
@@ -3,8 +3,13 @@ if not null_ls_status_ok then
   return
 end
 
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
+
 null_ls.setup {
-  debug = true,
+  debug = false,
   sources = {
     null_ls.builtins.formatting.google_java_format,
     null_ls.builtins.formatting.prettier,
