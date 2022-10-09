8d45fb05540801374641dafeb232774b66a1ebfd
imbacraft <dimbouzoukas@gmail.com>
2022-10-09 19:40:05 +0300

    Better highlight on yank

--- nvim/lua/plugins/autocommands.lua
+++ nvim/lua/plugins/autocommands.lua
@@ -1,69 +1,64 @@
+--Hide status line and tabline while on Dashboard screen
 vim.api.nvim_create_autocmd({ "User" }, {
-    pattern = { "AlphaReady" },
-    callback = function()
-        vim.cmd [[
+	pattern = { "AlphaReady" },
+	callback = function()
+		vim.cmd([[
       set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
       set laststatus=1 | autocmd BufUnload <buffer> set laststatus=3
-    ]]
-    end,
+    ]])
+	end,
 })
 
+--Close specific windows with q
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
-    callback = function()
-        vim.cmd [[
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+	callback = function()
+		vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR> 
       set nobuflisted 
-    ]]
-    end,
+    ]])
+	end,
 })
 
+--Set conceallevel and wrap for markdown files
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "markdown" },
-    callback = function()
-        vim.cmd [[
+	pattern = { "markdown" },
+	callback = function()
+		vim.cmd([[
       set conceallevel=2
       set wrap
-    ]]
-    end,
+      setlocal foldlevel=0
+      let g:markdown_fold_style = 'nested'
+    ]])
+	end,
 })
 
--- vim.api.nvim_create_autocmd({ "BufEnter" }, {
---     callback = function()
---         vim.cmd [[
---       if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
---     ]]
---     end,
--- })
-
--- vim.api.nvim_create_autocmd({ "VimResized" }, {
---     callback = function()
---         vim.cmd "tabdo wincmd ="
---     end,
--- })
---
---
--- vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
---     callback = function()
---         vim.cmd "set formatoptions-=cro"
---     end,
--- })
---
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-    callback = function()
-        vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
-    end,
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	callback = function()
+		vim.cmd([[
+      if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
+    ]])
+	end,
 })
 
--- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
---     pattern = { "*.java" },
---     callback = function()
---         vim.lsp.codelens.refresh()
---     end,
--- })
+--Don't autostart new comment line after a comment
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	callback = function()
+		vim.cmd("set formatoptions-=cro")
+	end,
+})
 
--- vim.api.nvim_create_autocmd({ "VimEnter" }, {
---     callback = function()
---         vim.cmd "hi link illuminatedWord LspReferenceText"
---     end,
--- })
+-- Highlight on yank
+local yankGrp = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	command = "silent! lua vim.highlight.on_yank()",
+	group = yankGrp,
+})
+
+--Refresh code actions
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	pattern = { "*.java" },
+	callback = function()
+		vim.lsp.codelens.refresh()
+	end,
+})
--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -152,10 +152,9 @@ local mappings = {
 
 	g = {
 		name = "Git",
-		j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-		k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+		n = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+		p = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
 		l = { "<cmd>GitBlameToggle<cr>", "Blame" },
-		p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
 		r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
 		R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
 		s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
@@ -168,7 +167,7 @@ local mappings = {
 		c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
 		d = {
 			"<cmd>Gitsigns diffthis HEAD<cr>",
-			"Diff",
+			"Git Diff",
 		},
 	},
 
