d183d9291eef65f070e78a86ef2397fadd26ea3b
imbacraft <dimbouzoukas@gmail.com>
2022-10-19 14:51:14 +0300

    Migration from lualine to duskline

---
+++ nvim/lua/plugins/duskline.lua
@@ -0,0 +1,40 @@
+-- This is a function to get the branch name
+local function branch_name()
+  local branch = io.popen("git rev-parse --abbrev-ref HEAD 2> /dev/null")
+  if branch then
+    local name = branch:read("*l")
+    branch:close()
+    if name then
+      return name
+    else
+      return ""
+    end
+  end
+end
+
+--This autocommand is necessary to keep branch name updated
+vim.api.nvim_create_autocmd({"FileType", "BufEnter", "FocusGained"}, {
+  callback = function()
+    vim.b.branch_name = branch_name()
+  end
+})
+
+function _G.status_line()
+  return " "
+    .. " "
+    .. branch_name()
+    .. "%="
+    .. "%t"
+    .. " "
+    .. "%h"
+    .. "%m"
+    .. "%="
+    .. "%{&filetype}"
+    .. "  "
+end
+
+vim.opt.statusline = "%{%v:lua.status_line()%}"
+
+-- Colors of the statusline
+vim.cmd("highlight statusline ctermfg=NONE ctermbg=NONE guifg=#33B1FF guibg=NONE")
+-- guifg=#525252
